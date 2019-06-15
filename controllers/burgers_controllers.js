17cf39fed842cfa29316037c93db3b0130f3d880
Loydith Valdivia <loydith@gmail.com>
2019-06-15 00:25:00 -0500

    make the post

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,15 +3,14 @@ var burger = require("../models/burger.js");
 var router = express.Router();
 
 router.get("/", function(req, res) {
-  res.redirect("/burgers");
-  // burger.all(function(data){
-  //   burger.all(function(data){
-   //  var allBurger ={
-    //   burgers: data,
-    // };
-   //  console.log(allBurger);
-    // res.render("index", allBurger);
-  //})
+  // res.render("/burgers");
+    burger.all(function(data){
+    var allBurger ={
+      burgers: data,
+    };
+    console.log(allBurger);
+    res.render("index", allBurger);
+  })
   
 });
 
@@ -25,14 +24,14 @@ router.get("/api/burgers", function(req, res) {
 
 router.post("/burgers/create", function(req, res) {
   var newBurger = req.body.burger_name;
-  burger.create(newBurger, function(res) {
-    console.log(res);
+  burger.create(newBurger, function(result) {
+    console.log(result);
     // res.status(200).end();
-    res.redirect("/");
+    res.redirect("/", newBurger);
   });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
+router.put("/api/burger/:id", function(req, res) {
   burger.update(req.params.id, function(res) {
     console.log(res);
     res.sendStatus(200);
