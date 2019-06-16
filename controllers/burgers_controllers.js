e2412b934f5305c0d703982a606a28267e01482f
Loydith Valdivia <loydith@gmail.com>
2019-06-15 23:23:40 -0500

    change the style in the index

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -3,14 +3,6 @@ var burger = require("../models/burger.js");
 var router = express.Router();
 
 router.get("/", function(req, res) {
-  // res.render("/burgers");
-    // burger.all(function(data){
-    // var allBurger ={
-    //   burgers: data,
-    // };
-  //   console.log(allBurger);
-  //   res.render("index", allBurger);
-  // })
   res.redirect("/burgers");
   
 });
@@ -19,7 +11,7 @@ router.get("/burgers", function(req, res) {
   // express callback 
   burger.all(function(burgerData) {
     res.render("index", { burger_data: burgerData });
-   //  res.json(data);
+   
   });
 });
 
@@ -30,12 +22,7 @@ router.post("/burgers/create", function(req, res) {
     console.log(result);
     res.redirect("/");
   })
-// }
-  // console.log(burgerData);
-    // (newBurger, function(result) {
-    // console.log(newBurger);
-    // // res.status(200).end();
-  // });
+
 });
 
 router.put("/burgers/:id", function(req, res) {
